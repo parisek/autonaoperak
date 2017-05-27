03d15e87e9fd9f128aeecaa62a43f1191cee7ba1
Petr Parimucha <petr@pari.cz>
2017-05-27 23:23:50 +0200

    Slider

---
+++ sites/all/modules/custom/customize/templates/customize-hp-slider.tpl.php
@@ -0,0 +1,22 @@
+<div class="owl-carousel owl-theme" id="main-slider">
+  <?php foreach ($slides as $slide): ?>
+    <div class="item" style="background-image:url('<?php print $slide['image']; ?>')">
+      <div class="item-inner">
+        <div class="text">
+          <?php print $slide['body']; ?>
+        </div>
+      </div>
+    </div>
+  <?php endforeach; ?>
+</div>
+<div class="slider-nav">
+  <div class="gradient-left"></div>
+  <div class="gradient-right"></div>
+  <div class="container">
+    <ul id='carousel-custom-dots' class='owl-dots'>
+      <?php foreach ($slides as $slide): ?>
+      <li class='owl-dot'><?php print $slide['anotation']; ?></li>
+      <?php endforeach; ?>
+    </ul>
+  </div>
+</div>
--- sites/all/themes/autonaoperak/js/script.js
+++ sites/all/themes/autonaoperak/js/script.js
@@ -24,6 +24,34 @@
         $('iframe', this).addClass('disable-iframe-move-js');
       });
 
+      var $slider = $('.slider-nav');
+      var $owl = $('#main-slider');
+
+      $owl.on('initialize.owl.carousel resize.owl.carousel', function(event) {
+        var width = $(window).width()-$('.container', $slider).width();
+        console.log(width);
+        $('.gradient-left', $slider).css('width', width/2 + 'px');
+        $('.gradient-right', $slider).css('width', width/2 + 'px');
+      })
+
+      $owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: ['<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>', '<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>'],
+        dots:true,
+        lazyLoad:true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause:true,
+        dotsContainer: '#carousel-custom-dots',
+      })
+
+      $('.owl-dot').on('click', function () {
+        $owl.trigger('to.owl.carousel', [$(this).index(), 300]);
+      });
+
       //toggle accordion up/down icon eg. faq section
       $('.panel-group .panel-collapse').on('hide.bs.collapse', function () {
         var $link = $(this).closest('.panel').find('.panel-title a');
