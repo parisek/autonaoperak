5c523c3faf03544246e87e8c7e495c329342ae5e
Petr Parimucha <petr@pari.cz>
2017-07-31 12:52:29 +0200

    Small fixes

--- sites/all/modules/custom/customize/templates/customize-highlight.tpl.php
+++ sites/all/modules/custom/customize/templates/customize-highlight.tpl.php
@@ -1,10 +1,11 @@
 <div class="highlight-list">
   <div class="inner">
     <ul>
-      <li><span class="icon-car"></span><span class="text">Máme desítky vozů ihned k dispozici</span></li>
-      <li><span class="icon-wheel"></span><span class="text">Kalkulaci vám připravíme rychle</span></li>
-      <li><span class="icon-wallet"></span><span class="text">Kalkulace je bez skrytých nákladů</span></li>
-      <li><span class="icon-calc"></span><span class="text">Kalkulaci vám upravíme na míru dle vašich požadavků</span></li>
+      <li><span class="icon-car"></span><span class="text">Desítky vozů skladem ihned k dispozici</span></li>
+      <li><span class="icon-wheel"></span><span class="text">Nabídka na míru do druhého pracovního dne</span></li>
+      <li><span class="icon-wallet"></span><span class="text">Kalkulace na míru bez skrytých poplatků</span></li>
+      <li><span class="icon-calc"></span><span class="text">Největší variabilita operáku na trhu</span></li>
+      <li><span class="icon-partner"></span><span class="text">Zázemí silného partnera ALD Automotive</span></li>
     </ul>
   </div>
 </div>
