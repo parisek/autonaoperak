748a5493f0f3cae25fd92dad4af8b52d0b7ed21a
Petr Parimucha <petr@pari.cz>
2018-01-25 14:03:20 +0100

    Removed used cars from HP mobile

--- sites/all/modules/custom/customize/templates/customize-hp-slider.tpl.php
+++ sites/all/modules/custom/customize/templates/customize-hp-slider.tpl.php
@@ -32,26 +32,3 @@
     </ul>
   </div>
 </div>
-<?php
-global $language;
-?>
-<?php if($language->language == 'cs'): ?>
-<div class="used-cars">
-  <div class="item-list">
-    <div class="image">
-      <img src="/sites/all/themes/autonaoperak/images/rocni-vozy.jpg" class="img-responsive" alt="">
-    </div>
-    <div class="text">
-      <div class="text-inner">
-      <h2>Roční vozy</h2>
-      <ul>
-        <li>Jsou <strong>ihned k dispozici</strong> = Zítra můžete jezdit</li>
-        <li>Dají se pronajmout na dobu <strong>od 1 roku</strong></li>
-        <li>Tato akce trvá <strong>jen do konce roku 2017</strong></li>
-      </ul>
-      <div class="morelink"><a href="<?php print customize_get_translate_link('node/443'); ?>" class="btn btn-primary">Zobrazit nabídku</a></div>
-      </div>
-    </div>
-  </div>
-</div>
-<?php endif; ?>
