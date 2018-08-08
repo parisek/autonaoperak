931dadf95ca5fd3050b289c520aea8e0c5b0dabd
Petr Parimucha <petr@pari.cz>
2018-08-08 15:38:56 +0200

    SK

--- sites/all/modules/custom/customize/templates/customize-highlight.tpl.php
+++ sites/all/modules/custom/customize/templates/customize-highlight.tpl.php
@@ -1,11 +1,11 @@
 <div class="highlight-list">
   <div class="inner">
     <ul>
-      <li><span class="icon-car"></span><span class="text">Desítky vozů skladem ihned k dispozici</span></li>
-      <li><span class="icon-wheel"></span><span class="text">Nabídka na míru do druhého pracovního dne</span></li>
-      <li><span class="icon-wallet"></span><span class="text">Kalkulace na míru bez skrytých poplatků</span></li>
-      <li><span class="icon-calc"></span><span class="text">Největší variabilita operáku na trhu</span></li>
-      <li><span class="icon-partner"></span><span class="text">Zázemí silného partnera ALD Automotive</span></li>
+      <li><span class="icon-car"></span><span class="text"><?php print t('Dozens of cars in stock available immediately'); ?></span></li>
+      <li><span class="icon-wheel"></span><span class="text"><?php print t('Tailored offer on the second business day'); ?></span></li>
+      <li><span class="icon-wallet"></span><span class="text"><?php print t('Tailor-made calculation without hidden charges'); ?></span></li>
+      <li><span class="icon-calc"></span><span class="text"><?php print t('The greatest variability of the operator on the market'); ?></span></li>
+      <li><span class="icon-partner"></span><span class="text"><?php print t('Based on the strong partner of ALD Automotive'); ?></span></li>
     </ul>
   </div>
 </div>
